83236e227f8ee4b50178ee25aadeee08d12dd0cb
Amnetr <leisure_0913@163.com>
2019-05-14 12:17:09 +0800

    init

---
+++ components/swipper.vue
@@ -0,0 +1,42 @@
+<template>
+  <md-card>
+    <md-card-media>
+      <!-- swiper -->
+      <swiper :options="swiperOption">
+        <swiper-slide class="swipper-img"></swiper-slide>
+        <swiper-slide class="swipper-img"></swiper-slide>
+        <swiper-slide class="swipper-img"></swiper-slide>
+        <swiper-slide class="swipper-img"></swiper-slide>
+        <swiper-slide class="swipper-img"></swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </md-card-media>
+  </md-card>
+</template>
+
+<script>
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+export default {
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination',
+          dynamicBullets: true
+        }
+      }
+    }
+  },
+  components: {
+    swiper,
+    swiperSlide
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.swipper-img
+  background-image: url('../static/img/timg.jpeg')
+  height: 8rem
+</style>
---
+++ pages/Index.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="main-container">
+    <div class="user-info"></div>
+    <div class="swipper-container">
+      <swipper></swipper>
+    </div>
+    <div class="nav-container">
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="12">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="8">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="4">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="4">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="4">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="16">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="4">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+      </el-row>
+    </div>
+    <div class="chart-container"></div>
+  </div>
+</template>
+
+<script>
+import swipper from '@/components/swipper.vue'
+export default {
+  name: 'index',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+  components: {
+    swipper
+  }
+}
+</script>
+
+<style lang="stylus">
+.main-container
+  height: 96%
+  width: 100%
+.user-info
+  width: 100%
+  height: 5%
+  margin-bottom: 0.5rem
+  background-color: blue
+.swipper-container
+  width: 100%
+  margin-bottom: 0.5rem
+.nav-container
+  width: 100%
+  height: 40%
+  background-color: green
+  margin-bottom: 0.5rem
+.chart-container
+  width: 100%
+  background-color: yellow
+  height: 30%
+.bg-purple
+  background: #eee
+.grid-content
+  border-radius: 4px
+  min-height: 36px
+</style>
